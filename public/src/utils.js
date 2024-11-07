a349ca677fb990afae38edd46d33c6218689779d
ilijas19 <ilijagocic19@gmail.com>
2024-11-07 16:54:59 +0100

    origin url fix

--- public/src/utils.js
+++ public/src/utils.js
@@ -1,4 +1,4 @@
-export const ORIGIN = "http://localhost:5000";
+// export const ORIGIN = "http://localhost:5000";
 
 //PRODUCTION URL
-// export const ORIGIN = "https://realtime-chatapp-2-0.onrender.com";
+export const ORIGIN = "https://realtime-chatapp-2-0.onrender.com";
